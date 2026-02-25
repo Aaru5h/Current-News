31370c0000f2cee6e531d1540c5e80e42d7a2ad2
Bendicion <editzjedidiah@gmail.com>
2026-02-25 19:57:01 +0300

    Implement data fetching functions for macro and stock data
    Added functions to fetch US macro data, stock quotes, and SEC filings.

--- bckend/src/services/market.services.js
+++ bckend/src/services/market.services.js
@@ -1,1 +1,50 @@
+import axios from "axios"; 
 
+const FRED_BASE = "https://api.stlouisfed.org/fred/series/observations";
+
+// US Macro Data (GDP + Inflation)
+export const fetchUSMacroData = async () => {
+  const gdp = await axios.get(FRED_BASE, {
+    paramas: {
+      series_id: "GDP",
+      api_key: process.env.FRED_API_KEY,
+      file_type: "json"
+    }
+  });
+
+const inflation = await axios.get(FRED_BASE, {
+  paramas: {
+    series_id: "CPIAUCSL",
+    api_key: process.env.FRED_API_KEY,
+    file_type: "json"
+  }
+});
+
+  return {
+    gdp: gdp.data.observation.slice(-5),
+    inflation: inflation.data.observation.slice(-5)
+  };
+};
+
+//Stock Quote (Using Yahoo Finance unofficial API)
+export const fetchStockQuote = async (symbol) => {
+  const response = await axios.get(
+     `https://query1.finance.yahoo.com/v7/finance/quote?symbols=${symbol}`
+  );
+
+  return response.data.quoteResponce.results[0];
+};
+
+//SEC EDGAR Filings 
+export const fetchSECData = async (cik) => {
+  const response = await axios.get(
+   `https://data.sec.gov/submissions/CIK${cik}.json`,
+    {
+      header: {
+        "User-Agent": "Current-News-App (your_email@example.com)"
+      }
+    }
+  );
+
+  return response.data.filings.recent;
+};
