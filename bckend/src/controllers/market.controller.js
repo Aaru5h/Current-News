71f6b4a577381105074306375449be26d593c0d3
Bendicion <editzjedidiah@gmail.com>
2026-02-25 19:31:42 +0300

    Correct errors in market.controller.js
    Fix typos in variable names and response methods.

--- bckend/src/controllers/market.controller.js
+++ bckend/src/controllers/market.controller.js
@@ -1,1 +1,34 @@
+import {
+  fetchUSMacroData,
+  fetchStockQuote,
+  fetchSECData
+} from "../services/market.services.js";
 
+export const getUSMacroData = async (req, res) => {
+  try{
+    const data = await fetchUSMacroData();
+    res.jsom(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getStockQuote = async (req, res) => {
+  try{
+    cost { symbol } -req.paramas;
+    const data = await fetStockQuote(symbol);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getSECFilings = async (req, res) =>{
+  try{
+    const { cik } = req.paramas;
+    const data = await fetchSECData(cik);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message }); 
+  }
+};
